a33745c44d2182513bd5cfa6ca714648e1895eb4
Umama Masroor <umamafatima319@gmail.com>
2023-05-10 21:10:00 -0700

    question 2

---
+++ question # 2/add.js
@@ -0,0 +1,9 @@
+// question 2
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+let visits = 0;
+visits++;
+alert("You have visited this site " + visits + " times.");
