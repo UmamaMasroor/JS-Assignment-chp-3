0c1a53db73e779ad3c350d269376c7a792c412bd
Umama Masroor <umamafatima319@gmail.com>
2023-05-12 16:24:05 -0700

    question 4

---
+++ question # 4/add.js
@@ -0,0 +1,15 @@
+// question # 4
+// A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+let visitorName ='John-Doe';
+let productTitle ='T-Shirt(s)';
+let Quantity ='5';
+document.write( visitorName+ " ordered " +Quantity+ " " +productTitle+ " on Urban Clothing Store")
